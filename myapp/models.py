ccbc269a155074ddfb8ab47499cf656ed97eb70d
MelodicTechno <617179025@qq.com>
2025-03-10 10:59:04 +0800

    后端增加获取商户统计数据

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class SimpleUser(models.Model):
+    uid = models.CharField(max_length=255)
+    level = models.CharField(max_length=255)
+
+    def __str__(self):
+        return str(self.id)
