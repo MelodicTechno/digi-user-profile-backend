d3f09461039e7d2faee520e1381740f05c890833
Wangender <128113610+Wangender@users.noreply.github.com>
2025-03-11 16:42:01 +0800

    评论修改

--- spark_app/utils/analyse.py
+++ spark_app/utils/analyse.py
@@ -816,19 +816,23 @@ def update_review():
         .getOrCreate()
 
     # 第一问
-    from pyspark.sql.functions import col, regexp_replace, lower, split, explode, size
-    from pyspark.ml.feature import StopWordsRemover
-    hive_df = spark.sql("SELECT * FROM default.review")
-    # 将字符串格式的日期转为date格式
-    updated_review = hive_df.withColumn("date", to_date(col("date"), "yyyy-MM-dd")) \
-        .withColumn("year", year(col("date")))
+    year_review_counts = spark.sql("""
+        SELECT
+            YEAR(TO_DATE(date, 'yyyy-MM-dd')) AS year,
+            COUNT(*) AS review_count
+        FROM
+            default.review
+        WHERE
+            YEAR(TO_DATE(date, 'yyyy-MM-dd')) IS NOT NULL
+        GROUP BY
+            YEAR(TO_DATE(date, 'yyyy-MM-dd'))
+        ORDER BY
+            YEAR(TO_DATE(date, 'yyyy-MM-dd'))
+    """)
 
-    # 将 updated_review 注册为临时视图
-    updated_review.createOrReplaceTempView("updated_review")
-
-    # 统计每年的评论数
-    year_review_counts = spark.sql("select year, count(*) as review_counts from updated_review group by year").collect()
+    year_review_counts = year_review_counts.collect()
     year_review_counts = [row.asDict() for row in year_review_counts]
+
 
     # 统计有用（helpful）、有趣（funny）及酷（cool）的评论及数量
     # 有用
@@ -885,120 +889,119 @@ def update_review():
     top_20_words = [row.asDict() for row in top_20_words_df]
 
     # 从评论中提取正面评论（评分>3）的Top10词语
-
-    from pyspark.sql.functions import col, regexp_replace, lower, split, explode, size
-    from pyspark.ml.feature import StopWordsRemover
-
-    # 提取正面评论
-    positive_reviews_df = spark.sql("SELECT * FROM review WHERE stars >= 3.0")
-
-    # 转换为小写并去除标点符号
-    positive_reviews_df = positive_reviews_df.withColumn(
-        "cleaned_text",
-        regexp_replace(lower(col("text")), "[^a-zA-Z\\s]", "")  # 去除非字母字符
-    )
-
-    # 分词
-    positive_reviews_df = positive_reviews_df.withColumn(
-        "words",
-        split(col("cleaned_text"), "\\s+")  # 按空格分词
-    )
-
-    # 过滤掉 words 列为 null 或空列表的行
-    positive_reviews_df = positive_reviews_df.filter(col("words").isNotNull() & (size(col("words")) > 0))
-
-    # 去除停用词
-    stop_words = StopWordsRemover.loadDefaultStopWords("english")  # 加载英文停用词
-    stop_words_remover = StopWordsRemover(inputCol="words", outputCol="filtered_words", stopWords=stop_words)
-    positive_reviews_df = stop_words_remover.transform(positive_reviews_df)
-
-    # 过滤掉空字符串
-    positive_reviews_df = positive_reviews_df.withColumn(
-        "filtered_words",
-        expr("filter(filtered_words, word -> word != '')")  # 过滤掉空字符串
-    )
-
-    # 展开词语
-    words_df = positive_reviews_df.select(explode(col("filtered_words")).alias("word"))
-
-    # 统计词频
-    word_counts_df = words_df.groupBy("word").count()
-
-    # 按词频排序并提取 Top 10
-    top_10_words_df = word_counts_df.orderBy(col("count").desc()).limit(10).collect()
-    top_10_words = [row.asDict() for row in top_20_words_df]
+    #
+    # from pyspark.sql.functions import col, regexp_replace, lower, split, explode, size
+    # from pyspark.ml.feature import StopWordsRemover
+    #
+    # # 提取正面评论
+    # positive_reviews_df = spark.sql("SELECT * FROM review WHERE stars >= 3.0")
+    #
+    # # 转换为小写并去除标点符号
+    # positive_reviews_df = positive_reviews_df.withColumn(
+    #     "cleaned_text",
+    #     regexp_replace(lower(col("text")), "[^a-zA-Z\\s]", "")  # 去除非字母字符
+    # )
+    #
+    # # 分词
+    # positive_reviews_df = positive_reviews_df.withColumn(
+    #     "words",
+    #     split(col("cleaned_text"), "\\s+")  # 按空格分词
+    # )
+    #
+    # # 过滤掉 words 列为 null 或空列表的行
+    # positive_reviews_df = positive_reviews_df.filter(col("words").isNotNull() & (size(col("words")) > 0))
+    #
+    # # 去除停用词
+    # stop_words = StopWordsRemover.loadDefaultStopWords("english")  # 加载英文停用词
+    # stop_words_remover = StopWordsRemover(inputCol="words", outputCol="filtered_words", stopWords=stop_words)
+    # positive_reviews_df = stop_words_remover.transform(positive_reviews_df)
+    #
+    # # 过滤掉空字符串
+    # positive_reviews_df = positive_reviews_df.withColumn(
+    #     "filtered_words",
+    #     expr("filter(filtered_words, word -> word != '')")  # 过滤掉空字符串
+    # )
+    #
+    # # 展开词语
+    # words_df = positive_reviews_df.select(explode(col("filtered_words")).alias("word"))
+    #
+    # # 统计词频
+    # word_counts_df = words_df.groupBy("word").count()
+    #
+    # # 按词频排序并提取 Top 10
+    # top_10_words_df = word_counts_df.orderBy(col("count").desc()).limit(10).collect()
+    # top_10_words = [row.asDict() for row in top_20_words_df]
 
 
     # 计算单词的关系图（譬如chinese、steak等单词）
     # 从 Hive 表中读取数据
-    reviews_df = spark.sql("SELECT * FROM review")
-
-    # 转换为小写并去除标点符号
-    reviews_df = reviews_df.withColumn(
-        "cleaned_text",
-        regexp_replace(lower(col("text")), "[^a-zA-Z\\s]", "")
-    )
-
-    # 分词
-    reviews_df = reviews_df.withColumn(
-        "words",
-        split(col("cleaned_text"), "\\s+")
-    )
-
-    # 定义滑动窗口
-    window_spec = Window.partitionBy("review_id").orderBy("word_index")
-
-    from pyspark.sql import functions as F
-    # 为每个单词添加索引
-    reviews_df = reviews_df.withColumn(
-        "word_index",
-        F.row_number().over(window_spec)
-    )
-
-    # 提取单词对
-    word_pairs_df = reviews_df.withColumn(
-        "word_pairs",
-        F.expr("""
-            transform(
-                sequence(1, size(words) - 1),
-                i -> array(words[i - 1], words[i])
-            )
-        """)
-    ).select(explode("word_pairs").alias("word_pair"))
-
-    # 过滤掉无效单词对
-    word_pairs_df = word_pairs_df.filter(
-        (col("word_pair")[0] != "") & (col("word_pair")[1] != "")
-    )
-
-    # 统计共现频率
-    co_occurrence_df = word_pairs_df.groupBy("word_pair").count()
-
-    # 过滤掉低频共现对
-    co_occurrence_df = co_occurrence_df.filter(col("count") > 5)
-
-
-    nodes = []
-    edges = []
-
-    # 添加边和权重
-    for row in co_occurrence_df.collect():
-        word1, word2 = row["word_pair"]
-        count = row["count"]
-
-        # 添加节点
-        if word1 not in nodes:
-            nodes.append({"name": word1})
-        if word2 not in nodes:
-            nodes.append({"name": word2})
-
-        edges.append({"source": word1, "target": word2, "value": count})
-
-    # 转换为 JSON 格式
-    graph_data = {
-        "nodes": nodes,
-        "edges": edges
-    }
+    # reviews_df = spark.sql("SELECT * FROM review")
+    #
+    # # 转换为小写并去除标点符号
+    # reviews_df = reviews_df.withColumn(
+    #     "cleaned_text",
+    #     regexp_replace(lower(col("text")), "[^a-zA-Z\\s]", "")
+    # )
+    #
+    # # 分词
+    # reviews_df = reviews_df.withColumn(
+    #     "words",
+    #     split(col("cleaned_text"), "\\s+")
+    # )
+    #
+    # # 定义滑动窗口
+    # window_spec = Window.partitionBy("review_id").orderBy("word_index")
+    #
+    # from pyspark.sql import functions as F
+    # # 为每个单词添加索引
+    # reviews_df = reviews_df.withColumn(
+    #     "word_index",
+    #     F.row_number().over(window_spec)
+    # )
+    #
+    # # 提取单词对
+    # word_pairs_df = reviews_df.withColumn(
+    #     "word_pairs",
+    #     F.expr("""
+    #         transform(
+    #             sequence(1, size(words) - 1),
+    #             i -> array(words[i - 1], words[i])
+    #         )
+    #     """)
+    # ).select(explode("word_pairs").alias("word_pair"))
+    #
+    # # 过滤掉无效单词对
+    # word_pairs_df = word_pairs_df.filter(
+    #     (col("word_pair")[0] != "") & (col("word_pair")[1] != "")
+    # )
+    #
+    # # 统计共现频率
+    # co_occurrence_df = word_pairs_df.groupBy("word_pair").count()
+    #
+    # # 过滤掉低频共现对
+    # co_occurrence_df = co_occurrence_df.filter(col("count") > 5)
+    #
+    # nodes = []
+    # edges = []
+    #
+    # # 添加边和权重
+    # for row in co_occurrence_df.collect():
+    #     word1, word2 = row["word_pair"]
+    #     count = row["count"]
+    #
+    #     # 添加节点
+    #     if word1 not in nodes:
+    #         nodes.append({"name": word1})
+    #     if word2 not in nodes:
+    #         nodes.append({"name": word2})
+    #
+    #     edges.append({"source": word1, "target": word2, "value": count})
+    #
+    # # 转换为 JSON 格式
+    # graph_data = {
+    #     "nodes": nodes,
+    #     "edges": edges
+    # }
 
     spark.stop()
 
@@ -1006,6 +1009,6 @@ def update_review():
         'year_review_counts': year_review_counts,
         'user_review_counts': user_review_counts,
         'top_20_words': top_20_words,
-        'top_10_words': top_10_words,
-        'graph_data': graph_data,
+        # 'top_10_words': top_10_words,
+        # 'graph_data': graph_data,
     }
